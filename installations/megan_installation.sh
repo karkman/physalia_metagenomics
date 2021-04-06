c50bce90274bc11a96868c4ad7fe9953cc91911e
Antti Karkman <antti.karkman@gmail.com>
2021-04-06 19:10:02 +0300

    more installations

---
+++ installations/megan_installation.sh
@@ -0,0 +1,6 @@
+wget https://software-ab.informatik.uni-tuebingen.de/download/megan6/MEGAN_Community_unix_6_21_2.sh
+
+sh MEGAN_Community_unix_6_21_2.sh
+
+wget https://software-ab.informatik.uni-tuebingen.de/download/megan6/megan-map-Jan2021.db.zip
+unzip megan-map-Jan2021.db.zip

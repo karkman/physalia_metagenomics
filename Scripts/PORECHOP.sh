d877b768f6b54a220f13fd26de7e28e8205a22e9
igorspp <igor.pessi@gmail.com>
2021-04-16 11:27:39 +0300

    Update PORECHOP.sh

--- Scripts/PORECHOP.sh
+++ Scripts/PORECHOP.sh
@@ -2,8 +2,8 @@
 conda activate QC_env
 
 for SAMPLE in Sample01 Sample02 Sample03 Sample04; do
-  porechop -i RAWDATA/$SAMPLE.NANOPORE.fastq \
-           -o TRIMMED/$SAMPLE.NANOPORE.fastq \
+  porechop -i RAWDATA/$SAMPLE.NANOPORE.fastq.gz \
+           -o TRIMMED/$SAMPLE.NANOPORE.fastq.gz \
            --discard_middle \
            --threads 4 > TRIMMED/$SAMPLE.porechop.log.txt
 done

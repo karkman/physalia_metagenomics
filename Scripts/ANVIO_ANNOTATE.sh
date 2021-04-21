8f573f48686e56ba23322a38536cfef47c301ce3
igorspp <igor.pessi@gmail.com>
2021-04-21 14:55:15 +0300

    Create ANVIO_ANNOTATE.sh

---
+++ Scripts/ANVIO_ANNOTATE.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+conda activate anvio-7
+
+for SAMPLE in Sample01 Sample02 Sample03 Sample04; do
+  anvi-run-ncbi-cogs --contigs-db BINNING_MEGAHIT/$SAMPLE/CONTIGS.db \
+                     --num-threads 20 &> BINNING_MEGAHIT/$SAMPLE.annotate.log.txt
+
+  anvi-run-kegg-kofams --contigs-db BINNING_MEGAHIT/$SAMPLE/CONTIGS.db \
+                       --num-threads 20 &>> BINNING_MEGAHIT/$SAMPLE.annotate.log.txt
+
+  anvi-run-pfams --contigs-db BINNING_MEGAHIT/$SAMPLE/CONTIGS.db \
+                 --num-threads 20 &>> BINNING_MEGAHIT/$SAMPLE.annotate.log.txt
+done
